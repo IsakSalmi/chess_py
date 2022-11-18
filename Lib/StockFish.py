6b38b82188766cf2d8710b1863469f1887414e41
isakSalmi <hansisak11@gmail.com>
2022-11-18 22:01:35 +0100

    some smalle changes

--- Lib/StockFish.py
+++ Lib/StockFish.py
@@ -1,10 +1,12 @@
-from stockfish import *
+from stockfish import Stockfish
 
 
 
 class StockFish():
     def __init__(self):
-        stockfish = Stockfish(path = r'//stockfish_15_win//stockfish_15_x64.exe')
+        stockfish = Stockfish(path = 'Lib/stockfish_15_win_x64_avx2/stockfish_15_x64_avx2.exe')
+        stockfish.set_depth(10);
+        stockfish.set_skill_level(20)
         
 
 
